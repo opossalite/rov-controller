8d2041047ca7c13c7f67d39887d82809cf56f7e4
terrior <terrior@CobraVM.CobaltCanidPC>
2024-04-10 07:20:51 +0000

    Got analog stick to control light

--- src/sketch_mar27a/sketch_mar27a.ino
+++ src/sketch_mar27a/sketch_mar27a.ino
@@ -8,42 +8,27 @@
 //bool buttonState = 0;       // variable to hold the button state
 //bool Mode = 0;
 //bool currentvalue = 0;
+const int PIN = 10;
 bool buttonA_state = false;
 
 void setup() {
   Serial.begin(9600);  // start serial communication at 9600 baud
   //pinMode(buttonPin, INPUT_PULLUP);    // Set the switch pin as input
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(PIN, OUTPUT);
+  analogWrite(PIN, 128); //ensure we start at middle position
 
 }
-void loop()
-{
-  if (Serial.read() == 1) {
-    //currentvalue = !currentvalue;
-    //digitalWrite(LED_BUILTIN, currentvalue);
-  //digitalWrite(LED_BUILTIN, HIGH);
-  //delay(1000);
-    buttonA_state = !buttonA_state;
-    digitalWrite(LED_BUILTIN, buttonA_state);
-    //if (buttonA_state) {
-    //  digitalWrite(LED_BUILTIN, HIGH);
-    //} else {
-    //  digitalWrite(LED_BUILTIN, LOW);
-    //}
+void loop() {
+  //if (Serial.read() == 1) {
+  //  buttonA_state = !buttonA_state;
+  //  digitalWrite(PIN, buttonA_state);
+  //}
+  int thing = Serial.read();
+  //Serial.println(thing);
+  if (thing == -1) {
+    return;
   }
-  
-  //digitalWrite(LED_BUILTIN, HIGH);
-  //delay(500);
-  //digitalWrite(LED_BUILTIN, LOW);
-  //delay(500);
+  analogWrite(PIN, thing);
 
-} 
-    
-    
-    
-    //if (Serial.read() != currentvalue){
-      //digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-      //delay(500);
-     // }
-  //  } 
-//}
+}
+
--- xbox_to_arduino.py
+++ xbox_to_arduino.py
@@ -4,7 +4,8 @@
 # Created by John Woolsey on 12/17/2019.
 # Copyright (c) 2019 Woolsey Workshop.  All rights reserved.
 # USB_PORT = "/dev/ttyUSB0"  # Arduino Uno R3 Compatible
-USB_PORT = "COM3"  # Arduino Uno WiFi Rev2
+#USB_PORT = "COM3"  # Arduino Uno WiFi Rev2
+USB_PORT = "/dev/ttyACM0"  # Arduino Uno WiFi Rev2
 # Imports
 import serial
 from inputs import get_gamepad
@@ -13,22 +14,23 @@ from inputs import get_gamepad
 # Main
 # Connect to USB serial port at 9600 baud
 try:
-   usb = serial.Serial(USB_PORT, 9600, timeout=2)
-except:
-   print("ERROR - Could not open USB serial port.  Please check your port name and permissions.")
-   print("Exiting program.")
-   exit()
+    usb = serial.Serial(USB_PORT, 9600, timeout=2)
+except Exception as e:
+    print(e)
+    print("ERROR - Could not open USB serial port.  Please check your port name and permissions.")
+    print("Exiting program.")
+    exit()
 
 #A_state = 0
 
 while True:
-   events = get_gamepad()
-   for event in events:
-      #print(event.ev_type, event.code, event.state)
-      if event.code == "BTN_SOUTH" and event.state == 1:
-         #A_state = not A_state
-         #print(A_state)
-         #usb.write(A_state.to_bytes())
-         usb.write((1).to_bytes())
+    events = get_gamepad()
+    for event in events:
+        #if event.code == "BTN_SOUTH" and event.state == 1:
+        #    usb.write((1).to_bytes())
+        if event.code == "ABS_X":
+            new_val = int((event.state + 32768) >> 8)
+            #print(new_val.to_bytes())
+            usb.write(new_val.to_bytes())
 
 
